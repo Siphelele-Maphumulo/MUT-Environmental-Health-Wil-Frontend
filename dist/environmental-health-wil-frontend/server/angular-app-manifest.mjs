11646515cbde22c1cb8b16c6675116cda78dc0e6
Siphelele A Maphumulo <siphelelemaphumulo@gmail.com>
2025-07-18 03:00:35 +0200

    Deploy fixed index.html for GitHub Pages

--- dist/environmental-health-wil-frontend/server/angular-app-manifest.mjs
+++ dist/environmental-health-wil-frontend/server/angular-app-manifest.mjs
@@ -14,8 +14,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 134360, hash: 'fc091151fad0571358530189fcf5b5bb26e557ec8a7b91fed74b64848609c6ae', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 33316, hash: '58de2712b258d14d44dc31a142301bcbc653028970bd733401746d48ca77486f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 134360, hash: '2973e7e9b17182b86e1c333d7ef6e2567abf6824a7ead2e732aacaf9ccf5f38b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 33316, hash: '40cb8029057fb66deaf227163a846aade01cb3d29a9e946b9197c3c7d0d72879', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-BF54USQ5.css': {size: 383295, hash: 'DqKqZMot1Zk', text: () => import('./assets-chunks/styles-BF54USQ5_css.mjs').then(m => m.default)}
   },
 };
