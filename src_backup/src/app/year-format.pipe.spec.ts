4f3a035ad5e540de82c214fec5020f8c939a9f24
Siphelele A Maphumulo <siphelelemaphumulo@gmail.com>
2025-10-13 13:03:27 +0200

    Changing to MongoDB

--- src_backup/src/app/year-format.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { YearFormatPipe } from './year-format.pipe';
-
-describe('YearFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new YearFormatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src_backup/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api', // Update with your actual API base URL
-};
